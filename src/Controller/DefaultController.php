bc04d2c3e6d317e304e315db414e249efd570cae
Irene Vinader <irene.v1995@gmail.com>
2025-01-27 12:49:39 +0100

    Vistas y rutas

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    #[Route('/', name: 'index')]
     public function index()
     {
-        return $this->render('base.html.twig');
+        return $this->render('index.view.html.twig');
+    }
+
+    #[Route('/about', name: 'about')] //Se podrá acceder desde navegador poniendo sym.local/about
+    public function abaut() {
+        return $this->render('about.html.twig');
     }
 }
